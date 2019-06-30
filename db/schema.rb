a1df27a3556cbed0ea4ac71fa8a139ff477c6f09
Anna Haley <anna.lk.haley@gmail.com>
2019-06-29 21:59:13 -0400

    more postgresql stuuf

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20190628193345) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "entries", force: :cascade do |t|
     t.string "name"
     t.string "inout"
